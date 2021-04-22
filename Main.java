14c86171f9715b95c329c865ef356a550adba229
loreleitrim <69655840+loreleitrim@users.noreply.github.com>
2021-04-22 15:39:54 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,12 +4,21 @@ public class Main {
 	
 	public static void main (String args[]) {
 		GOL p = GOL.makeGOLonRing(5);
+
 		int t = 0;
 		do {
 			System.out.println("" + p.toString());
+			Pattern._sequence.add(p.toString());
 			p.next();
 			t++;
-		} while (t < 10);
+		} while (t < 15);
+		
+		int patternLength = Pattern.patternLength(Pattern._sequence);
+	
+		
+    	System.out.println(patternLength);
+    	
+    	
 	}
 
 }
